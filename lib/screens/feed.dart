6f47d1e707b1137268afb8d0e858ec34107aa010
Afnan Hassan <50952934+HassanAfnan@users.noreply.github.com>
2020-06-09 02:02:17 +0500

    Add files via upload

---
+++ lib/screens/feed.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+import 'package:food/api/food_api.dart';
+import 'package:food/notifier/auth_notifier.dart';
+import 'package:food/notifier/food_notifier.dart';
+import 'package:food/screens/details.dart';
+import 'package:food/screens/food_form.dart';
+import 'package:provider/provider.dart';
+
+class Feed extends StatefulWidget {
+  @override
+  _FeedState createState() => _FeedState();
+}
+
+class _FeedState extends State<Feed> {
+
+  @override
+  void initState() {
+    FoodNotifier foodNotifier = Provider.of<FoodNotifier>(context, listen: false);
+    getFoods(foodNotifier);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    AuthNotifier authNotifier = Provider.of<AuthNotifier>(context);
+    FoodNotifier foodNotifier = Provider.of<FoodNotifier>(context);
+
+    Future<void> _refreshList() async {
+      getFoods(foodNotifier);
+    }
+
+    print("building Feed");
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          authNotifier.user != null ? authNotifier.user.displayName : "Feed",
+        ),
+        actions: <Widget>[
+          // action button
+          FlatButton(
+            onPressed: () => signout(authNotifier),
+            child: Text(
+              "Logout",
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+      body: new RefreshIndicator(
+    child: ListView.separated(
+    itemBuilder: (BuildContext context, int index) {
+      return ListTile(
+        leading: Image.network(
+          foodNotifier.foodList[index].image != null
+              ? foodNotifier.foodList[index].image
+              : 'https://www.testingxperts.com/wp-content/uploads/2019/02/placeholder-img.jpg',
+          width: 120,
+          fit: BoxFit.fitWidth,
+        ),
+        title: Text(foodNotifier.foodList[index].name),
+        subtitle: Text(foodNotifier.foodList[index].category),
+        onTap: () {
+          foodNotifier.currentFood = foodNotifier.foodList[index];
+          Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+            return FoodDetail();
+          }));
+        },
+      );
+    },
+      itemCount: foodNotifier.foodList.length,
+      separatorBuilder: (BuildContext context, int index) {
+      return Divider(
+      color: Colors.black,
+      );
+     },
+    ),
+       onRefresh: _refreshList,
+    ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          foodNotifier.currentFood = null;
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (BuildContext context) {
+              return FoodForm(
+                isUpdating: false,
+              );
+            }),
+          );
+        },
+        child: Icon(Icons.add),
+        foregroundColor: Colors.white,
+      ),
+    );
+  }
+}
