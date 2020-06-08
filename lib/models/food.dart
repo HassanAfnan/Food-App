e50fda2ad64d0ce177fec00ae51ef42b5b96373a
Afnan Hassan <50952934+HassanAfnan@users.noreply.github.com>
2020-06-09 02:15:32 +0500

    Add files via upload

---
+++ lib/models/food.dart
@@ -0,0 +1,36 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Food {
+  String id;
+  String name;
+  String category;
+  String image;
+  List subIngredients = [];
+  Timestamp createdAt;
+  Timestamp updatedAt;
+
+  Food();
+
+  Food.fromMap(Map<String, dynamic> data) {
+    id = data['id'];
+    name = data['name'];
+    category = data['category'];
+    image = data['image'];
+    subIngredients = data['subIngredients'];
+    createdAt = data['createdAt'];
+    updatedAt = data['updatedAt'];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'category': category,
+      'image': image,
+      'subIngredients': subIngredients,
+      'createdAt': createdAt,
+      'updatedAt': updatedAt
+    };
+  }
+}
